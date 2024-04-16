844309865fa381f2071b7b94ab06d89ab1f6331f
Sai Vikhyath Kudhroli <vikhyath456@gmail.com>
2024-04-16 03:18:55 -0700

    complete: complete exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, number) {
+    let result = "";
+    while(number > 0) {
+        result = result + string;
+        number = number - 1;
+    }
+    return result;
 };
 
 // Do not edit below this line
