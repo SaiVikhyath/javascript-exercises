d158c9a31067f6ae144084e3ca04f9e939706ddb
Sai Vikhyath Kudhroli <vikhyath456@gmail.com>
2024-04-16 03:25:33 -0700

    complete: complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reverse = "";
+    for(let idx = string.length - 1; idx >= 0; idx--) {
+        reverse = reverse + string[idx];
+    }
+    return reverse;
 };
 
 // Do not edit below this line
