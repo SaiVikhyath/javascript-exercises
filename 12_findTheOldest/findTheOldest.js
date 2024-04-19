081a8f8c60dc87b1aba116a9e21a96e48d5be17a
Sai Vikhyath Kudhroli <vikhyath456@gmail.com>
2024-04-19 06:55:03 -0700

    complete: complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    
+    let oldestPerson;
+    let oldestPersonAge = 0;
+    
+    for (const person of people) {
+        let age = 0;
+        const date = new Date();
+        if (person.yearOfDeath === undefined) {
+            age = date.getFullYear() - person.yearOfBirth;
+        } else {
+            age = person.yearOfDeath - person.yearOfBirth;
+        }
+        if (age > oldestPersonAge) {
+            oldestPerson = person;
+            oldestPersonAge = age;
+        }
+    }
+    return oldestPerson;
 };
 
 // Do not edit below this line
