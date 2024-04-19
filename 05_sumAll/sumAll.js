5a080da4450d0f55f76c467e96e94559282d204f
Sai Vikhyath Kudhroli <vikhyath456@gmail.com>
2024-04-19 00:51:13 -0700

    complete: complete assignment 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,22 @@
-const sumAll = function() {
+const sumAll = function(startNumber, endNumber) {
+
+    if (typeof startNumber != "number" || typeof endNumber != "number") {
+        return "ERROR";
+    }
+
+    let start = Math.min(startNumber, endNumber);
+    let end = Math.max(startNumber, endNumber);
+
+    let sum = 0;
+
+    for(let counter = start; counter <= end; counter++) {
+        if (counter < 0) {
+            return "ERROR";
+        }
+        sum = sum + counter;
+    }
+
+    return sum;
 
 };
 
