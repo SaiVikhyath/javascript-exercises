83537694e1c6f54ffde807bd9a8d2b8164a5cedd
Sai Vikhyath Kudhroli <vikhyath456@gmail.com>
2024-04-19 06:00:27 -0700

    complete: complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,30 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+
+    if (number < 0) {
+        return "OOPS"
+    }
+    
+    if (number == 0) {
+        return 0;
+    }
+
+    if (number == 1 || number == 2) {
+        return 1;
+    }
+
+    let number1 = 1;
+    let number2 = 1;
+
+    let idx = 2;
+
+    do {
+        let temp = number1 + number2;
+        number1 = number2;
+        number2 = temp;
+        idx = idx + 1;
+    } while(idx != number);
+
+    return number2;
 
 };
 
