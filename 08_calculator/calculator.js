69e249df084d54485d8d0fefc94f0c62ed134edb
Sai Vikhyath Kudhroli <vikhyath456@gmail.com>
2024-04-19 01:28:57 -0700

    complete: complete exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(firstNumber, secondNumber) {
+  return firstNumber + secondNumber;
 };
 
-const subtract = function() {
-	
+const subtract = function(firstNumber, secondNumber) {
+  return firstNumber - secondNumber;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let result = 0;
+  for(let idx = 0; idx < array.length; idx++) {
+    result = result + array[idx];
+  }
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let result = 1;
+  for(let idx = 0; idx < array.length; idx++) {
+    result = result * array[idx];
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function(firstNumber, secondNumber) {
+	return Math.pow(firstNumber, secondNumber);
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  if (number == 0) {
+    return 1;
+  }
+  return number * factorial(number - 1);
 };
 
 // Do not edit below this line
