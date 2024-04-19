5573ff03ec6f6c30494e4cc86092d7f444c0ce0c
Sai Vikhyath Kudhroli <vikhyath456@gmail.com>
2024-04-19 01:38:13 -0700

    complete: complete exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,25 @@
-const palindromes = function () {
+const palindromes = function (string) {
+
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
+
+    string = string
+        .toLowerCase()
+        .split('')
+        .filter((character) => alphanumerical.includes(character))
+        .join('');
+
+    let start = 0;
+    let end = string.length - 1;
+
+    while (start <= end) {
+        if (string[start] != string[end]) {
+            return false;
+        }
+        start += 1;
+        end -= 1;
+    }
+
+    return true;
 
 };
 
